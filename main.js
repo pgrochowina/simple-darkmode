989906291c7c0cd996721515576c47aa7d923dec
Przemysław Grochowina <pgrochowina@gmail.com>
2020-06-12 09:40:04 +0200

    styling inspired by overreacted.io

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const mainHeader = document.querySelector(".header--js");
 
 viewMode.addEventListener("click", () => {
   if (isDark == false) {
-    document.documentElement.style.setProperty("--background-color", "black");
-    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "#282c35");
+    document.documentElement.style.setProperty("--text-color", "#ffa7c4");
     isDark = true;
     mainHeader.innerHTML = 'To jest przykładowy tryb ciemny';
     console.log(`zmieniono tryb na ciemny, wartość isDark = ${isDark}`);
   } else if (isDark == true) {
     document.documentElement.style.setProperty("--background-color", "white");
-    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--text-color", "#d23669");
     isDark = false;
     mainHeader.innerHTML = 'To jest przykładowy tryb jasny';
     console.log(`zmieniono tryb na jasny, wartość isDark = ${isDark}`);
