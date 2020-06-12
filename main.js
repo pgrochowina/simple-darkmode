2bb797f28cf9bbafa4f17854e50f6364785dc4c1
Przemysław Grochowina <pgrochowina@gmail.com>
2020-06-12 09:25:44 +0200

    JavaScript if statment done

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Test połączenia do JS.')
+let isDark = false;
+
+const viewMode = document.querySelector(".button__toggle--js");
+const mainHeader = document.querySelector(".header--js");
+
+viewMode.addEventListener("click", () => {
+  if (isDark == false) {
+    document.documentElement.style.setProperty("--background-color", "black");
+    document.documentElement.style.setProperty("--text-color", "white");
+    isDark = true;
+    console.log(`zmieniono tryb na ciemny, wartość isDark = ${isDark}`);
+  } else if (isDark == true) {
+    document.documentElement.style.setProperty("--background-color", "white");
+    document.documentElement.style.setProperty("--text-color", "black");
+    isDark = false;
+    console.log(`zmieniono tryb na jasny, wartość isDark = ${isDark}`);
+  }
+});
