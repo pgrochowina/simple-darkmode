f144093be10bba08939578ca1b2fb4fa80910d7e
Przemysław Grochowina <pgrochowina@gmail.com>
2020-06-12 09:29:05 +0200

    JS code to change header added

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ viewMode.addEventListener("click", () => {
     document.documentElement.style.setProperty("--background-color", "black");
     document.documentElement.style.setProperty("--text-color", "white");
     isDark = true;
+    mainHeader.innerHTML = 'To jest przykładowy tryb ciemny';
     console.log(`zmieniono tryb na ciemny, wartość isDark = ${isDark}`);
   } else if (isDark == true) {
     document.documentElement.style.setProperty("--background-color", "white");
     document.documentElement.style.setProperty("--text-color", "black");
     isDark = false;
+    mainHeader.innerHTML = 'To jest przykładowy tryb jasny';
     console.log(`zmieniono tryb na jasny, wartość isDark = ${isDark}`);
   }
 });
